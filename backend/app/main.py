7f78ff1aa1e07c8f1df73212049b1b699431f83e
harshiit1 <bhardwajharshit171@gmail.com>
2026-04-17 00:25:45 +0530

    Added changes to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,17 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.routes.upload import router as uploadRouter
 from app.routes.jobs import router as jobsRouter
 from app.routes.webSocket import router as webSocketRouter 
 from app.routes.document import router as documentRouter
 from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
+from app.db import Base, engine
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield  
 
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
@@ -17,8 +23,3 @@ app.include_router(uploadRouter)
 app.include_router(jobsRouter)
 app.include_router(webSocketRouter)
 app.include_router(documentRouter)
-
-
-
-# from app.db import Base, engine
-# Base.metadata.create_all(bind=engine)
